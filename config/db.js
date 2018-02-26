d01c0723be1f3c3cf380d4d50602ceb5558c0f3c
Andreya <avalabek@yahoo.com>
2018-02-25 21:26:57 -0700

    routes working, sequelize working

--- config/db.js
+++ config/db.js
@@ -1,32 +1,42 @@
-var Sequelize = require ('sequelize');
+// var db = require ("db");
+var Sequelize = require ("sequelize");
 //is this necessary below
-// var env = require ('./env');
-var sequelize = new Sequelize(
-  env.DATABASE_NAME,
-  env.DATABASE_USERNAME,
-  env.DATABASE_PASSWORD,
-  {
-    host: env.DATABASE_HOST,
-    port: env.DATABASE_PORT,
-    dialect: env.DATABASE_DIALECT,
-    define: {
-      underscored: true
-    }
+//  var env = require ('./env');
+var sequelize = new Sequelize("sequelize_kitchen", "root", "root",{
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
   }
+}
+  // env.DATABASE_NAME,
+  // env.DATABASE_USERNAME,
+  // env.DATABASE_PASSWORD,
+  // {
+  //   host: env.DATABASE_HOST,
+  //   port: env.DATABASE_PORT,
+  //   dialect: env.DATABASE_DIALECT,
+  //   define: {
+  //     underscored: true
+  //   }
+  // }
 );
-var db = {};
+module.exports = sequelize;
+//   var db = {};
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+//  db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
 
-// models==list all??
-db.hosts = require ("../models/hosts.js")(sequelize,Sequelize);
-db.guests = require("../models/guests.js")(sequelize, Sequelize);
-db.events = require("../models/events.js")(sequelize, Sequelize);
+// // models==list all??
+// db.hosts = require ("../models/hosts.js")(sequelize,Sequelize);
+// db.guests = require("../models/guests.js")(sequelize, Sequelize);
+// db.events = require("../models/events.js")(sequelize, Sequelize);
 
-//relationships
-db.guests.belongsTo(db.hosts);
-db.hosts.hasMany(db.guests);
-db.hosts.belongsTo(db.events);
+// //relationships
+// db.guests.belongsTo(db.hosts);
+// db.hosts.hasMany(db.guests);
+// db.hosts.belongsTo(db.events);
 
-module.exports = db;
+// module.exports = db;
