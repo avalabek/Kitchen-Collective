749a62bf0dbf8ead3fb8678945567160dc9578fb
Andreya <avalabek@yahoo.com>
2018-02-26 13:49:33 -0700

    add guest and host models, decouple js from html

---
+++ models/guests.js
@@ -0,0 +1,35 @@
+var Sequelize = require("sequelize");
+var sequelize = require("../config/db.js");
+var DataTypes = require("sequelize/lib/data-types");
+module.exports = function(sequelize, DataTypes) {
+  var Guests = sequelize.define(
+    "Guests",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV1,
+        primaryKey: true
+      },
+      event: {
+        type: DataTypes.STRING
+      },
+
+      name: {
+        type: DataTypes.STRING
+      },
+      email: {
+        type: DataTypes.STRING
+      },
+      phone: {
+        type: DataTypes.INTEGER
+      }
+      
+      
+      //TODO check date/time datatypes
+    },
+    {
+      timestamps: false
+    }
+  );
+  return Guests;
+};
