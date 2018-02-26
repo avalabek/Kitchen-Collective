c9c145f8882ca03b1058b299adab407ae48c0292
Mike Morales <mikemorale@live.com>
2018-02-25 22:49:03 -0700

    donate

--- kitchen_collective.js
+++ kitchen_collective.js
@@ -1,13 +1,14 @@
 var mysql = require ('mysql');
-var inquirer = require('inquirer');
+var inquirer = require ('inquirer');
+
 var connection = mysql.createConnection({
 	host:"localhost",
-	port: 3306,
+	port:3306,
 	user:"root",
 	password:"dkamka13",
-	database:"KCEvent_DB"
-})
-connection.connect (function(err){
-	console.log("Connected as id: "+connection.threadId);
+	database:"KCEventdb",
 })
 
+connection.connect(function(err){
+	console.log("Connected as id:"+connection,threadId);
+}) 
