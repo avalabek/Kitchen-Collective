554bd2af50bf8325ca5ac7f860277b88224e3667
Andreya <avalabek@yahoo.com>
2018-02-26 18:03:22 -0700

    update host routes

--- routes/routes/hosts.js
+++ routes/routes/hosts.js
@@ -1,49 +1,55 @@
+var express = require("express");
+var app = express.Router();
+var db = require("../../models/hosts.js");
+
 module.exports = function (app, db){
+
+  app.get("/", function(req,res){
+    db.Hosts.findAll()
+    .then(function(data){
+      var hosts = {
+        hosts: data
+      };
+      console.log(hosts);
+      res.render("index", hosts);
+    });
+  });
+  
   //GET all hosts
-  app.get ("/hosts", function (req,res){
-    db.hosts.findAll()
+  app.get ("/api/hosts", function (req,res){
+    db.Hosts.findAll()
     .then(function(hosts){
       res.json(hosts);
     });
   });
 //GET host by event
-  app.get("/hosts:event", function(req, res) {
-    var event = req.params.id;
-    db.host
-      .find({
-        where: { event: event }
-      })
-      .then(function(event) {
-        res.json(event);
-      });
-  });
+  // app.get("/hosts:event", function(req, res) {
+  //   var event = req.params.id;
+  //   db.host
+  //     .find({
+  //       where: { event: event }
+  //     })
+  //     .then(function(event) {
+  //       res.json(event);
+  //     });
+  // });
 //changed this to match input on page
   //POST one host so when host is posted
   //event also needs to be posted. 
-  app.post("/host", function(req, res) {
-    var firstname = req.body.firstname;
-    var lastname = req.body.lastname;
-    var cuisine = req.body.cuisine;
-    var email = req.body.email;
-    var address = req.body.address;
-    var date = req.body.date;
-    var peoplecount = req.body.peoplecount;
-    
-    
-    db.hosts
-      .create({
-        //verify that these are in db
-        firstname: firstname,
-        lastname: lastname,
-        cuisine: cuisine,
-        email: email,
-        address: address,
-        date: date,
-        peoplecount: peoplecount
+  app.post("api/hosts", function(req, res) {
+    db.Hosts.create({
+      firstname: req.body.firstname,
+       lastname: req.body.lastname,
+      cuisine: req.body.cuisine,
+       email: req.body.email,
+      address: req.body.address,
+      date: req.body.date,
+      peoplecount: req.body.peoplecount
+
+    }).then(function(result) {
+        res.json(result);
+        console.log("Your host: ", req.body.firstname);
       })
-      .then(function(newHost) {
-        res.json(newHost);
-      });
   });
   
   
