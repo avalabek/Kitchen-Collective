3f9f133617aa289a1f0492f976d02b0ec0f57b7c
Andreya <avalabek@yahoo.com>
2018-03-01 15:55:33 -0700

    modal appears when guest list button clicked

--- public/js/guest.js
+++ public/js/guest.js
@@ -3,7 +3,10 @@ $(function () {
 
   $(document).ready(function(){
     $('.slider').slider();
-    });
+
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+ 
 
   
   $("#register").on("click", function (event) {
@@ -20,7 +23,7 @@ $(function () {
     console.log(newGuest);
 
   
-    // Send the PUT request.
+    // Send the POST request.
 
 
     $.ajax("/api/guests/", {
@@ -54,9 +57,57 @@ $(function () {
     //   location.reload();
     //   });       
   });
+//When guestlist button on Lebanese card is clicked, calls to db, modal opens with info
+  $("#guestlist_leb").on("click", function (event) {
+    event.preventDefault();
+    // Function for creating a new list row for authors
+    function createAuthorRow(authorData) {
+      console.log(authorData);
+      var newTr = $("<tr>");
+      newTr.data("author", authorData);
+      newTr.append("<td>" + authorData.name + "</td>");
+      newTr.append("<td># of posts will display when we learn joins in the next activity!</td>");
+      newTr.append("<td><a href='/blog?author_id=" + authorData.id + "'>Go to Posts</a></td>");
+      newTr.append("<td><a href='/cms?author_id=" + authorData.id + "'>Create a Post</a></td>");
+      newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
+      return newTr;
+    }
+
+    // Function for retrieving authors and getting them ready to be rendered to the page
+    function getAuthors() {
+      $.get("/api/authors", function (data) {
+        var rowsToAdd = [];
+        for (var i = 0; i < data.length; i++) {
+          rowsToAdd.push(createAuthorRow(data[i]));
+        }
+        renderAuthorList(rowsToAdd);
+        nameInput.val("");
+      });
+    }
+
+    // A function for rendering the list of authors to the page
+    function renderAuthorList(rows) {
+      authorList.children().not(":last").remove();
+      authorContainer.children(".alert").remove();
+      if (rows.length) {
+        console.log(rows);
+        authorList.prepend(rows);
+      }
+      else {
+        renderEmpty();
+      }
+    }
 
 
 
+
+//open modal once data has been retrieved
+    $('#modal1').modal('open');
+    
+    });
+
+    
+  });//end of document.ready
 });//end of iife
 
 
